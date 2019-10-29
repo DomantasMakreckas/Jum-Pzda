2121b8179a72dee0b271bdf17e7e179f4352a40e
TheRealRobis <retasas.ruskys@gmail.com>
2019-10-29 12:28:22 +0200

    data su php

--- index.php
+++ index.php
@@ -2,11 +2,11 @@
 <html>
 <head>
   <meta charset="utf-8">
-  <title><?php print 'Aš,' . date('l') . " ir PHP";?></title>
+  <title>PHP lydes ir <?php print date('d F Y', strtotime('+1 day')) ;?></title>
 </head>
 <body>
-    <h1>Robertas Ruskys - HTML, <?php print 'Ir PHP';?> . asas jau nuo . <?php print date('Y') . ' metu';?>  </h1>
-    <p>Viskas prasidejo <?php print date('F') . ' menesio ' . date('l') . " diena!";?></p>
+    <h1>Robertas - PHP su manim buvo ir  <?php print date('h', strtotime('-1 hour')) . ' valanda!';?></h1>
+    <p><?php print date('Y', strtotime('+1 year'));?> ne uz kalnu!</p>
 </body>
 </html>
 
