59bc1dc3362d2397fb57b16992b6732ccab0e7d8
Ugnius <ugniusnaujokas@gmail.com>
2019-10-29 11:55:36 +0200

    Trecia uzduotis

--- index.php
+++ index.php
@@ -4,6 +4,6 @@
     </head>
     <body>
         <h1><strong>Ugnius</strong> - HTML <?php print 'ir PHP' ?> asas jau nuo <?php print date("y") . ' metų'; ?></h1>
-        <p>Viskas prasidejo <?php print date("F"). ' mėnesio, '.date("d")." dieną!"; ?></p>
+        <p>Viskas prasidejo <?php print date("F") . ' mėnesio, ' . date("d") . " dieną!"; ?></p>
     </body>
 </html>
