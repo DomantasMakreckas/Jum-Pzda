cb4936550f5629645866b893723503e3158e9cee
Ugnius <ugniusnaujokas@gmail.com>
2019-10-29 12:25:26 +0200

    Ketvirta uzduotis

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <html>
     <head>
-        <title><?php print 'Aš, ' . date("l") . ' ir PHP'; ?></title>
+        <title>PHP lydės ir <?php print date("Y-m-d", strtotime("+1 day")); ?></title>
     </head>
     <body>
-        <h1><strong>Ugnius</strong> - HTML <?php print 'ir PHP' ?> asas jau nuo <?php print date("y") . ' metų'; ?></h1>
-        <p>Viskas prasidejo <?php print date("F") . ' mėnesio, ' . date("d") . " dieną!"; ?></p>
+        <h1><strong>Ugnius</strong> - PHP su manim ir  <?php print date('g', strtotime("-1 hour")) . ' valandą!'; ?></h1>
+        <p><?php print date('Y', strtotime("+1 year")); ?> ne už kalnų</p>
     </body>
 </html>
