ea4177a2b447167ceb6193d11e8d728b1b5cac48
TheRealRobis <retasas.ruskys@gmail.com>
2019-10-29 10:17:03 +0200

    html karkasas sukurtas

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
-<?php 
+<html>
 
-print "veikia";
+<head>
+  <meta charset="utf-8">
+  <title>Aš valdau HTML</title>
+
+</head>
+
+<body>
+
+    <h1>Robertas Ruskys</h1>
+    <p>Moku kurti statini turini.</p>
+    
+</body>
+
+</html>
