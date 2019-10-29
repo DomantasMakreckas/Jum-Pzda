171b5cb3e804b84e2c24940ec8354eb6c81c6eec
TheRealRobis <retasas.ruskys@gmail.com>
2019-10-29 11:07:15 +0200

    php kodas html bodyje

--- index.php
+++ index.php
@@ -1,16 +1,12 @@
-<html>
 
+<html>
 <head>
   <meta charset="utf-8">
-  <title>Aš valdau HTML</title>
-
+  <title>Aš valdau HTML <?php print 'Ir PHP';?></title>
 </head>
-
 <body>
-
-    <h1>Robertas Ruskys</h1>
-    <p>Moku kurti statini turini.</p>
-    
+    <h1>Robertas Ruskys - HTML, <?php print 'Ir PHP';?> </h1>
+    <p>Moku kurti statini turini. Atidaryta: <?php print date('Y-m-d H:i:s');?></p>
 </body>
-
 </html>
+
