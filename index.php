8ccfd164ae0be3ad223eb65e4bc5d8f6cb999d65
Ugnius <ugniusnaujokas@gmail.com>
2019-10-29 12:47:08 +0200

    Penkta uzduotis

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
-!<html>
+<html>
     <head>
         <title>Komentarai</title>
     </head>
     <body>
-        <h1>Dainų tekstai</h1>
+        <h1>Dainiu Tekstai</h1>
         <h2>Paragrafo Daina</h2>
-        <p>Aš toks vienišas jaunas paragrafas...</p>
+        <p>As toks vienisas jaunas paragrafas...</p>
+        <!--
         <p>Mindau svetimo kodo gatves...</p>
+        -->
     </body>
 </html>
