fc17ca2d3b417031f0f6333d834c86e3df03ecaf
Ugnius <ugniusnaujokas@gmail.com>
2019-10-30 10:13:35 +0200

    sesta uzduotis

--- index.php
+++ index.php
@@ -5,8 +5,16 @@
             body {
                 background-color: rgb(<?php print rand(0, 255); ?>, <?php print rand(0, 255); ?>,<?php print rand(0, 255); ?>);
             }
+            .textSize {
+                font-size: <?php print rand(1, 100); ?>;
+            }
+            .textColor {
+                color: rgb(<?php print rand(0, 255); ?>, <?php print rand(0, 255); ?>,<?php print rand(0, 255); ?>);
+            }
         </style>
     </head>
     <body>
+        <h1 class ="textSize">Aš keičiu savo dydį</h1>
+        <p class="textColor">Aš keičiu savo spalvą</p>
     </body>
 </html>
