32943af7157ff4d7684ba7d9fd922e8ef6004a7f
TheRealRobis <retasas.ruskys@gmail.com>
2019-10-30 11:25:54 +0200

    BOMBA DIDEJA

--- index.php
+++ index.php
@@ -2,11 +2,19 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <title>PHP lydes ir <?php print date('m-d-Y', strtotime('+' . rand(1, 10) . ' year'))?></title>
+        <title>PHP</title>
+        <style>             
+            .box {
+                width: <?php print date('s') * 50;?>;px;
+                height: <?php print date('s') * 50;?> 200px;
+            }
+        </style>
     </head>
     <body>
-        <h1><b>Robertas</b> - Galbut turesiu <?php print rand(1, 5)?> vaikus</h1>
-        <p>D. Trump`as nebebus perzidentu: <?php  print date('Y-m-d', strtotime('+'. rand(2, 10) . 'year'))?></p>
+        <img class="box" src="images.jpg">
+        
+        <h1><?php  print date('s')?></h1>
+        
     </body>
 </html>
 
