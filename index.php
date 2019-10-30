d476e89e08646d04c30cd9ca81d9e1f6fd42356b
TheRealRobis <retasas.ruskys@gmail.com>
2019-10-30 11:27:34 +0200

    bomba geriau dideja

--- index.php
+++ index.php
@@ -5,16 +5,14 @@
         <title>PHP</title>
         <style>             
             .box {
-                width: <?php print date('s') * 50;?>;px;
-                height: <?php print date('s') * 50;?> 200px;
+                width: <?php print date('s') * 15;?>;px;
+                height: <?php print date('s') * 15;?>;px;
             }
         </style>
     </head>
     <body>
         <img class="box" src="images.jpg">
-        
-        <h1><?php  print date('s')?></h1>
-        
+        <h1><?php  print date('s')?></h1>      
     </body>
 </html>
 
