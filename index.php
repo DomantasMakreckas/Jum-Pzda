a22ef1ebd0fbee12901e737789170c057689590f
Ugnius <ugniusnaujokas@gmail.com>
2019-10-30 10:43:56 +0200

    septinta uzduotis

--- index.php
+++ index.php
@@ -1,20 +1,8 @@
 <html>
     <head>
-        <title></title>
-        <style>
-            body {
-                background-color: rgb(<?php print rand(0, 255); ?>, <?php print rand(0, 255); ?>,<?php print rand(0, 255); ?>);
-            }
-            .textSize {
-                font-size: <?php print rand(1, 100); ?>;
-            }
-            .textColor {
-                color: rgb(<?php print rand(0, 255); ?>, <?php print rand(0, 255); ?>,<?php print rand(0, 255); ?>);
-            }
-        </style>
+        <title>Kauliukas</title>
     </head>
     <body>
-        <h1 class ="textSize">Aš keičiu savo dydį</h1>
-        <p class="textColor">Aš keičiu savo spalvą</p>
+        <img src="kauliukai/<?php print rand(0,6); ?>.png">
     </body>
 </html>
