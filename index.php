1e6227b8d46328a475b833d16be394916c10c5ab
Ugnius <ugniusnaujokas@gmail.com>
2019-10-30 12:16:44 +0200

    desimta uzduotis

--- index.php
+++ index.php
@@ -5,10 +5,21 @@
             .bomba {
                 width: <?php print date('s'); ?>%;
             }
+            .bombasprogus00 {
+                width: 0;
+                opacity: 0;
+            }
+            .sprogimas{
+                width: 0;
+            }
+            .bombasprogimas00 {
+                width: 100%;
+            }
         </style>
     </head>
     <body>
-        <img src="/assets/images/bomb.jpg" class ="bomba">
-        <?php print date('s'); ?>
+        <img src="assets/images/bomb.jpg" class="bomba bombasprogus<?php print date("s"); ?>">
+        <div class="bombasprogus<?php print date("s"); ?>"><?php print date('s'); ?></div>
+        <img class="bombasprogimas<?php print date("s"); ?> sprogimas" src="assets/images/explosion.jpg">
     </body>
 </html>
