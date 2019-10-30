80116a317872057c4d424c49122d6e227a6487fd
Ugnius <ugniusnaujokas@gmail.com>
2019-10-30 11:14:27 +0200

    devinta uzduotis

--- index.php
+++ index.php
@@ -3,11 +3,12 @@
         <title>Bomba</title>
         <style>
             .bomba {
-                width: <?php print date("s"); ?>%;
+                width: <?php print date('s'); ?>%;
             }
         </style>
     </head>
     <body>
         <img src="/assets/images/bomb.jpg" class ="bomba">
+        <?php print date('s'); ?>
     </body>
 </html>
