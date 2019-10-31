dd77c60f2272d88e70c4800d5e3e248fae0a1ee9
Ugnius <ugniusnaujokas@gmail.com>
2019-10-31 11:21:16 +0200

    keturiolikta uzduotis

--- index.php
+++ index.php
@@ -1,18 +1,21 @@
 <?php
 
-$img_src = 'http://qnimate.com/wp-content/uploads/2014/03/images2.jpg';
+    $sudas1 = (rand(1,100));
+    $sudas2 = (rand(1,100));
+    $sudas3 = (rand(1,100));
+    $sudas4 = (rand(1,100));
 
 ?>
-
-
 <html>
     <head>
         <title>Variables</title>
     </head>
     <body>
-        <img src="<?php print $img_src; ?>">
-        <img src="<?php print $img_src; ?>">
-        <img src="<?php print $img_src; ?>">
-        <img src="<?php print $img_src; ?>">
+        <div class="body1">
+            <h1>Skolos skaičiuoklė</h1>
+            <h3>Jei paėmei <?php print $sudas1; ?> jievru</h3>
+            <h4>Su dviem kabančiais grąžinsi <?php print $sudas3; ?></h4>
+            <h2>Su vienu kabančiu grąžinsi <?php print $sudas2; ?></h2>
+        </div>
     </body>
 </html>
