e633981dc9d8e9df1261657a279211aaefcd28c8
TheRealRobis <retasas.ruskys@gmail.com>
2019-10-31 12:10:26 +0200

    siuksliu labas

--- index.php
+++ index.php
@@ -1,16 +1,27 @@
 <?php
-    $img_src = 'https://i.kym-cdn.com/entries/icons/original/000/027/475/Screen_Shot_2018-10-25_at_11.02.15_AM.png';
-;?>
+$bin_vol = 40;
+$bin_heap_vol = rand(5, 420);
+$trash_per_day = 15;
+
+$days = floor(($bin_vol + $bin_heap_vol) / $trash_per_day);
+
+$h1 = "Siuksliu prognoze";
+$p_1 = "Turime siuksline - $bin_vol litru";
+$p_2 = "Zmona nieko nesako, kol kampas nevirsija $bin_heap_vol";
+$h3 = "Isvada: Nieko nedarysiu $days dienu"
+;
+?>
 <html>
     <head>
         <meta charset="utf-8">
         <title>PHP</title>
+
     </head>
     <body>
-        <img src="<?php print $img_src;?>">
-        <img src="<?php print $img_src;?>">
-        <img src="<?php print $img_src;?>">
-        <img src="<?php print $img_src;?>">
+        <h1><?php print $h1 ?></h1>
+        <p><?php print $p_1 ?></p>
+        <p><?php print $p_2 ?></p
+        <h3><?php print $h3 ?></h3>
     </body>
 </html>
 
