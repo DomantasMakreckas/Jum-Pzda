bd9a4c5074d95579e209302143fce2626a7903a7
Ugnius <ugniusnaujokas@gmail.com>
2019-10-31 10:52:48 +0200

    trylikta uzduotis

--- index.php
+++ index.php
@@ -7,9 +7,10 @@ $img_src = 'http://qnimate.com/wp-content/uploads/2014/03/images2.jpg';
 
 <html>
     <head>
-        <title>Bomba</title>
+        <title>Variables</title>
     </head>
     <body>
+        <img src="<?php print $img_src; ?>">
         <img src="<?php print $img_src; ?>">
         <img src="<?php print $img_src; ?>">
         <img src="<?php print $img_src; ?>">
