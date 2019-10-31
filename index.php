624a7c44cf9d9f49c7eef4610677a256f647af02
Ugnius <ugniusnaujokas@gmail.com>
2019-10-31 10:32:53 +0200

    dvylikta uzduotis

--- index.php
+++ index.php
@@ -1,25 +1,20 @@
 <html>
     <head>
         <title>Bomba</title>
-        <style>
-            .bomba {
-                width: <?php print date('s'); ?>%;
-            }
-            .bombasprogus00 {
-                width: 0;
-                opacity: 0;
-            }
-            .sprogimas{
-                width: 0;
-            }
-            .bombasprogimas00 {
-                width: 100%;
-            }
-        </style>
     </head>
     <body>
-        <img src="assets/images/bomb.jpg" class="bomba bombasprogus<?php print date("s"); ?>">
-        <div class="bombasprogus<?php print date("s"); ?>"><?php print date('s'); ?></div>
-        <img class="bombasprogimas<?php print date("s"); ?> sprogimas" src="assets/images/explosion.jpg">
+        <?php
+        $string = 'Zodis';
+        $int = 14;
+        $boolean = true;
+        $null = null;
+        ?>
+        <ul>
+            <li>String tipas: <?php print $string ?></li>
+            <li>Integer Tipas: <?php print $int ?></li>
+            <li>Boolean tipas: <?php print $boolean ?></li>
+            <li>Null tipas: <?php print $null ?></li>
+            <li></li>
+        </ul>
     </body>
 </html>
