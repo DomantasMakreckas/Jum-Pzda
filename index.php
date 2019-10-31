cbfec526705017869ab977c280bcfae108aee658
TheRealRobis <retasas.ruskys@gmail.com>
2019-10-31 12:37:10 +0200

    kelione su php

--- index.php
+++ index.php
@@ -1,27 +1,29 @@
 <?php
-$bin_vol = 40;
-$bin_heap_vol = rand(5, 420);
-$trash_per_day = 15;
 
-$days = floor(($bin_vol + $bin_heap_vol) / $trash_per_day);
+$distance = rand(50, 690);
+$consumption = 7.5;
+$price_1 = 1.3;
 
-$h1 = "Siuksliu prognoze";
-$p_1 = "Turime siuksline - $bin_vol litru";
-$p_2 = "Zmona nieko nesako, kol kampas nevirsija $bin_heap_vol";
-$h3 = "Isvada: Nieko nedarysiu $days dienu"
-;
-?>
+$fuel_total = round($distance / $consumption, 2) ;
+$price_trip = round($fuel_total * $price_1, 2) ;
+
+$h1 = "Keliones skaiciuokle";
+$li_1 = "Nuvaziota distancija: $distance";
+$li_2 = "Sunaudota $fuel_total l. kuro";
+$li_3 = "Kaina: $price_trip pinigu";
+;?>
 <html>
     <head>
         <meta charset="utf-8">
         <title>PHP</title>
-
     </head>
     <body>
-        <h1><?php print $h1 ?></h1>
-        <p><?php print $p_1 ?></p>
-        <p><?php print $p_2 ?></p
-        <h3><?php print $h3 ?></h3>
+        <h1><?php print $h1;?></h1>
+        <ul>
+            <li><?php print $li_1;?></li>
+            <li><?php print $li_2;?></li>
+            <li><?php print $li_3;?></li>            
+        </ul>
     </body>
 </html>
 
