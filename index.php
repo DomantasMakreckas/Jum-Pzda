9eed8e127635f95244a6be4a28fcd77dcd19343c
TheRealRobis <retasas.ruskys@gmail.com>
2019-11-04 17:36:52 +0200

    laikas

--- index.php
+++ index.php
@@ -1,41 +1,39 @@
 <?php
-$bool = true;
-$str = '1';
-$flt = 1.23;
-$int = 1;
-$str_1 = '1.23';
+$sekunde = date('s');
 
-if ($bool == $int) {
-    $li_1_ats = 'lygus';
-} if ($bool === $int) {
-    $li_1_ats = 'Identiski';
+if (($sekunde % 2) == 0) {
+    $h1 = 'lygine';
+    $class = 'kvadratas';
+} else {
+    $h1 = 'nelygine';
+    $class = 'apskritimas';
 }
-if ($str == $bool) {
-    $li_2_ats = 'lygus';
-} if ($str === $bool) {
-    $li_2_ats = 'Identiski';
-}
-if ($flt == $str_1) {
-    $li_3_ats = 'lygus';
-} if ($flt === $str_1) {
-    $li_3_ats = 'Identiski';
-}
-
-$li_1 = "Bool (true) ir Integer (1): $li_1_ats";
-$li_2 = "String (1) ir Boolean (1): $li_2_ats";
-$li_3 = "Float (1.23) ir String(1.23): $li_3_ats";
-?>
+;?>
 <html>
     <head>
         <meta charset="utf-8">
         <title>PHP</title>
+        <style>
+            div {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                width: 100px;
+                height: 100px;
+            }
+            .kvadratas {
+                border: 2px solid black;
+            }
+            .apskritimas {
+                border-radius: 50%;
+                border:2px solid black;
+            }
+        </style>         
     </head>
     <body>
-        <ul>
-            <li><?php print $li_1 ?></li>
-            <li><?php print $li_2 ?></li>
-            <li><?php print $li_3 ?></li>
-        </ul>
+        <div class="<?php print $class ?>">
+            <h1><?php print $sekunde ?></h1>
+        </div>
     </body>
 </html>
 
