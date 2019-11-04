2074cbf65752cac22ea6b3da3cbda453c623891a
TheRealRobis <retasas.ruskys@gmail.com>
2019-11-04 16:26:44 +0200

    orai php

--- index.php
+++ index.php
@@ -1,29 +1,36 @@
 <?php
-$grizai_velai = rand(0,1);
-$grizai_isgeres = rand(0,1);
-
-$h1 = 'Buitine skaiciuokle';
-$h2 = '';
+$sunny = rand(0, 1);
 
 
-if($grizai_isgeres && $grizai_velai) {
-    $h2 = 'Grizai velai ir isgeres';
-} if($grizai_isgeres && !$grizai_velai ) {
-    $h2 = 'Grizai isgeres';
-} if(!$grizai_isgeres && $grizai_velai) {
-    $h2 = 'Grizai velai';
-} if(!$grizai_isgeres && !$grizai_velai) {
-    $h2 = 'Nieko nepadarei';
+if ($sunny) {
+    $img = 'https://cdn3.iconfinder.com/data/icons/symbol-1-1/36/12-512.png';
+    $h1 = 'Sauleta';
+} else {
+    $img = 'https://previews.123rf.com/images/designofire/designofire1811/designofire181101801/112650027-cloudy-icon-for-personal-and-commercial-use-.jpg';
+    $h1 = 'Debesuota';
 }
-;?>
+?>
 <html>
     <head>
         <meta charset="utf-8">
         <title>PHP</title>
+        <style>
+            img {
+                width: 100px;
+                height: 100px;
+            }
+            
+            div  {
+                display: flex;
+            }
+        </style>
     </head>
     <body>
-        <h1><?php print $h1 ;?></h1>
-        <h2><?php print $h2 ;?></h2>
+        <div>
+            <h1><?php print $h1; ?></h1>
+            <img src=<?php print $img ?>>      
+        </div>
+
     </body>
 </html>
 
