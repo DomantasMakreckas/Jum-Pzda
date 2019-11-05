eccb5748bf0a2fce85316f456cffea1e23d66032
TheRealRobis <retasas.ruskys@gmail.com>
2019-11-05 16:09:20 +0200

    pinigai gali pasibaigti

--- index.php
+++ index.php
@@ -3,17 +3,21 @@ $months = 12;
 $wallet = 1000;
 $month_income = 700;
 
-for($x = 1; $x <= $months; $x++){
-    $wallet += $month_income - rand(1,690);
+for ($x = 1; $x <= $months; $x++) {
+    $wallet += $month_income - rand(100, 3000);
+    if ($wallet < 0) {
+        $h2 = "Atsargiai, $x menesi gali baigtis pinigai!";
+        break;
+    }
+    $h2 = "Po $months m., prognozuojamas likutis: $wallet";
 }
 
 $h1 = 'Wallet Forecast';
-$h2 = "Po $months m., prognozuojamas likutis: $wallet";
 ?>
 <html>
     <head>
         <meta charset="utf-8">
-        <title>PHP</title>          
+        <title>PHP</title>       
     </head>
     <body>
         <h1><?php print $h1; ?></h1>
