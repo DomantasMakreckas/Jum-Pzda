9f505f275f0676b66c6a28d787346025f7ee7d91
TheRealRobis <retasas.ruskys@gmail.com>
2019-11-05 14:27:40 +0200

    antras roundas

--- index.php
+++ index.php
@@ -1,19 +1,16 @@
 <?php
-$x = rand(1,10);
+$x = rand(1, 10);
 
-
-for($y = 0; $y < $x; $y++){
+for ($y = 0; $y < $x; $y++) {
     $h1 = "Tai yra $y-tasis ciklas";
     var_dump($h1);
 }
-;?>
+;
+?>
 <html>
     <head>
         <meta charset="utf-8">
         <title>PHP</title>          
     </head>
-    <body>
-        <h1><?php print $h1?></h1>
-    </body>
 </html>
 
