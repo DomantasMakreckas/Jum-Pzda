bcff1ca33314446b134000aaf25bb1d2a2b42a33
TheRealRobis <retasas.ruskys@gmail.com>
2019-11-05 15:46:11 +0200

    wallet alga islaidos

--- index.php
+++ index.php
@@ -1,21 +1,14 @@
 <?php
-$kates = rand(1, 3);
-$sunys = rand(1, 3);
-$katasuniai = 0;
+$months = 12;
+$wallet = 1000;
+$month_income = 700;
 
-for ($k = 1; $k <= $kates; $k++) {
-    for ($s = 1; $s <= $sunys; $s++) {
-        $pavyko = rand(0, 1);
-        if ($pavyko == 1) {
-            $katasuniai++;
-            break;
-        }
-    }
+for($x = 1; $x <= $months; $x++){
+    $wallet = $wallet + $month_income - rand(1,690);
 }
 
-$h1 = 'Katasuniu Iseiga';
-$h2 = "Dalyvavo $kates kates ir $sunys sunys";
-$h3 = "Katasuniu iseiga: $katasuniai";
+$h1 = 'Wallet Forecast';
+$h2 = "Po $months m., prognozuojamas likutis: $wallet";
 ?>
 <html>
     <head>
@@ -25,7 +18,6 @@ $h3 = "Katasuniu iseiga: $katasuniai";
     <body>
         <h1><?php print $h1; ?></h1>
         <h2><?php print $h2; ?></h2>
-        <h3><?php print $h3; ?></h3>
     </body>
 </html>
 
