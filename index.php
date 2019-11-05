d6d1fc54553d70450ede67133ae8bef65e6e984b
TheRealRobis <retasas.ruskys@gmail.com>
2019-11-05 14:25:54 +0200

    ciklai pirma pamoka

--- index.php
+++ index.php
@@ -1,39 +1,19 @@
 <?php
-$sekunde = date('s');
+$x = rand(1,10);
 
-if (($sekunde % 2) == 0) {
-    $h1 = 'lygine';
-    $class = 'kvadratas';
-} else {
-    $h1 = 'nelygine';
-    $class = 'apskritimas';
+
+for($y = 0; $y < $x; $y++){
+    $h1 = "Tai yra $y-tasis ciklas";
+    var_dump($h1);
 }
 ;?>
 <html>
     <head>
         <meta charset="utf-8">
-        <title>PHP</title>
-        <style>
-            div {
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                width: 100px;
-                height: 100px;
-            }
-            .kvadratas {
-                border: 2px solid black;
-            }
-            .apskritimas {
-                border-radius: 50%;
-                border:2px solid black;
-            }
-        </style>         
+        <title>PHP</title>          
     </head>
     <body>
-        <div class="<?php print $class ?>">
-            <h1><?php print $sekunde ?></h1>
-        </div>
+        <h1><?php print $h1?></h1>
     </body>
 </html>
 
