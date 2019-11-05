b933611bca0d593b127c59eaed868343e9a9b570
TheRealRobis <retasas.ruskys@gmail.com>
2019-11-05 15:00:31 +0200

    kates ir sunys

--- index.php
+++ index.php
@@ -1,15 +1,31 @@
 <?php
-$x = rand(1, 10);
+$kates = rand(1, 3);
+$sunys = rand(1, 3);
+$katasuniai = 0;
 
-for ($y = 0; $y < $x; $y++) {
-    
-    var_dump("Tai yra $y-tasis ciklas");
+for($k = 0; $k <= $kates; $k++){
+    for($s = 0; $s <= $sunys; $s++){
+        $pavyko = rand(0, 1);
+        if($pavyko == 1){
+            $katasuniai++;
+            break;
+        }
+    }
 }
+
+$h1 = 'Katasuniu Iseiga';
+$h2 = "Dalyvavo $kates kates ir $sunys sunys";
+$h3 = "Katasuniu iseiga: $katasuniai";
 ?>
 <html>
     <head>
         <meta charset="utf-8">
         <title>PHP</title>          
     </head>
+    <body>
+        <h1><?php print $h1;?></h1>
+        <h2><?php print $h2;?></h2>
+        <h3><?php print $h3;?></h3>
+    </body>
 </html>
 
