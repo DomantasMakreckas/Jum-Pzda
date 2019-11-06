28af2dc39f8e7c4e5310bcd22e9946db965fe14a
Ugnius <ugniusnaujokas@gmail.com>
2019-11-06 11:51:29 +0200

    dvidesimtpirma uzduotis

--- index.php
+++ index.php
@@ -1,14 +1,20 @@
 <?php
-$petrol_in_tank = rand(20, 30);
-$h1_1 = 'Pripylei dyzelio į benz. mašina?';
-$h2_1 = "Benzino buvo: $petrol_in_tank l.";
+$months = 24;
+$car_price_new = 30000; // eur
+$desprecation = 2; // men
+$h1_1 = 'Kiek nuvertės mašina?';
+$h2_1 = "Naujos mašinos kaina: $car_price_new";
 
-$diesel_limit = $petrol_in_tank / 10 * 100;
-for ($dyzelis = 0; $dyzelis < $diesel_limit; $dyzelis++) {
-    $diesel_limit = ($petrol_in_tank + $dyzelis) / 100 * 10;
+
+$car_price_used= $car_price_new;
+for($i = 0; $i < $months; $i++)
+{
+    $car_price_used =  $car_price_used - ($car_price_new/100 * $desprecation);
 }
+$depr_perc = 100-($car_price_used * 100 / $car_price_new);
+$h3_1 = "Po $months mėn, mašinos vertė bus: $car_price_used eur";
+$h4_1 = "Mašina nuvertės $depr_perc proc";
 
-$h3_1 = "Max dyzelio riba: $diesel_limit l";
 ?>
 <html>
     <head>
@@ -18,5 +24,6 @@ $h3_1 = "Max dyzelio riba: $diesel_limit l";
         <h1><?php print $h1_1; ?></h1>
         <h2><?php print $h2_1; ?></h2>
         <h3><?php print $h3_1; ?></h3>
+        <h4><?php print $h4_1; ?></h4>
     </body>
 </html>
