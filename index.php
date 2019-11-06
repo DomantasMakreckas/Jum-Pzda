0013b68e21250e05751455a1b31032c6c1c32248
Ugnius <ugniusnaujokas@gmail.com>
2019-11-06 11:13:09 +0200

    dvidesimta uzduotis

--- index.php
+++ index.php
@@ -1,37 +1,22 @@
 <?php
+$petrol_in_tank = rand(20, 30);
+$h1_1 = 'Pripylei dyzelio į benz. mašina?';
+$h2_1 = "Benzino buvo: $petrol_in_tank l.";
 
-$distance = rand(100, 500); // km
-$consumption = 7.5; // l/100km
-$price_l = 1.3; // Eur/l
-$fuel_total = round($distance / 100 * $consumption, 2);
-$price_trip = round($fuel_total * $price_l, 2);
-$my_money = 100; // eur
-
-$h1_1 = 'Kelionės skaičiuoklė';
-$li_1 = "Nuvažiuota distancija: $distance";
-$li_2 = "Sunaudota $fuel_total l. kuro.";
-$li_3 = "Kaina: $price_trip pinigų";
-$li_4 = "Turimi pinigai: $my_money";
-if ($my_money >= $price_trip) {
-    $h1_2 = 'įperkama';
-} else {
-    $h1_2 = 'neįperkama';
+$diesel_limit = $petrol_in_tank / 10 * 100;
+for ($dyzelis = 0; $dyzelis < $diesel_limit; $dyzelis++) {
+    $diesel_limit = ($petrol_in_tank + $dyzelis)/100*10;
 }
-$p_1 = "Išvada: Kelionė $h1_2";
 
+$h3_1 = "Max dyzelio riba: $diesel_limit l";
 ?>
 <html>
     <head>
-        <title>Kuro skaičiuoklė</title>
+        <title>Lyginimas</title>
     </head>
     <body>
         <h1><?php print $h1_1; ?></h1>
-        <ul>
-            <li><?php print $li_1; ?></li>
-            <li><?php print $li_2; ?></li>
-            <li><?php print $li_3; ?></li>
-            <li><?php print $li_4; ?></li>
-        </ul>
-        <p><?php print $p_1; ?>
+        <h2><?php print $h2_1; ?></h2>
+        <h3><?php print $h3_1; ?></h3>
     </body>
 </html>
