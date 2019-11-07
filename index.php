092524668338f98a32917d1096cb718b53da5da1
TheRealRobis <retasas.ruskys@gmail.com>
2019-11-07 17:01:02 +0200

    mario kart

--- index.php
+++ index.php
@@ -1,60 +1,6 @@
 <?php
 
-$array = [
-// PIRMAS EILE
-    [
-        'Privemta' => false,
-//  Kegliu busena
-// Kegliu eiles prasideda nuo pirmo matoto
-// Kegliai skaiciuojami nuo kaires i desine
-        [
-            [
-                true,
-            ],
-            [
-                false,
-                true,
-            ],
-            [
-                false,
-                false,
-                true,
-            ],
-            [
-                true,
-                false,
-                false,
-                true,
-            ],
-        ],
-    ],
-//  ANTRA EILE
-    [
-        'Privemta' => true,
-//  Kegliu busena
-// Kegliu eiles prasideda nuo pirmo matoto
-// Kegliai skaiciuojami nuo kaires i desine
-        [
-            [
-                false,
-            ],
-            [
-                false,
-                false,
-            ],
-            [
-                false,
-                false,
-                false,
-            ],
-            [
-                false,
-                false,
-                false,
-                false,
-            ],
-        ],
-    ]
-];
+$array = [90,15,69,420,123,720];
+
 var_dump($array);
 
