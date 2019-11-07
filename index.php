7d3ee3963ba3d0aa05b6687681c5dfe0c1143187
TheRealRobis <retasas.ruskys@gmail.com>
2019-11-07 16:06:39 +0200

    maksyma

--- index.php
+++ index.php
@@ -1,44 +1,40 @@
 <?php
 
 $array = [
-    'lentyna_1' => [
-        'produktas' => [
-            [
-                'pavadinimas' => 'Kiausiniai',
-                'kiekis' => 3,
-            ],
-            [
-                'pavadinimas' => 'Grietine',
-                'kiekis' => 1,
-            ],
-            [
-                'pavadinimas' => 'Jogurtas',
-                'kiekis' => 2,
-            ],
-            [
-                'pavadinimas' => 'Zuvis',
-                'kiekis' => 0,
-            ],
+    [
+        [
+            'pavadinimas' => 'Kiausiniai',
+            'kiekis' => 3,
+        ],
+        [
+            'pavadinimas' => 'Grietine',
+            'kiekis' => 1,
+        ],
+        [
+            'pavadinimas' => 'Jogurtas',
+            'kiekis' => 2,
+        ],
+        [
+            'pavadinimas' => 'Zuvis',
+            'kiekis' => 0,
         ],
     ],
-    'lentyna_2' => [
-        'produktai' => [
-            'produktas' => [
-                'pavadinimas' => 'Pienas',
-                'kiekis' => 1,
-            ],
-            'produktas' => [
-                'pavadinimas' => 'Kefyras',
-                'kiekis' => 0,
-            ],
-            'produktas' => [
-                'pavadinimas' => 'Degtine',
-                'kiekis' => 2,
-            ],
-            'produktas' => [
-                'pavadinimas' => 'Kecupas',
-                'kiekis' => 2,
-            ],
+    [
+        [
+            'pavadinimas' => 'Pienas',
+            'kiekis' => 1,
+        ],
+        [
+            'pavadinimas' => 'Kefyras',
+            'kiekis' => 0,
+        ],
+        [
+            'pavadinimas' => 'Degtine',
+            'kiekis' => 2,
+        ],
+        [
+            'pavadinimas' => 'Kecupas',
+            'kiekis' => 2,
         ],
     ],
 ];
