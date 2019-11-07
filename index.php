bc7888a097b60d6d570527bc18521555d699bce2
TheRealRobis <retasas.ruskys@gmail.com>
2019-11-07 14:45:02 +0200

    maistas

--- index.php
+++ index.php
@@ -4,17 +4,17 @@ $array = [
     [
         'produktas' => 'kiausiniai',
         'dydis' => 'Didelis',
-        'kiekis' => '1'
+        'kiekis' => 1
     ],
     [
         'produktas' => 'baklazanas',
         'dydis' => 'Mazas',
-        'kiekis' => '2'
+        'kiekis' => 2
     ],
     [
         'produktas' => 'Grietiniles indelis',
         'dydis' => 'Didelis',
-        'kiekis' => '1'
+        'kiekis' => 1
     ],
 ];
 
