44f83ede65dfd677d3180bce9c1362b98317ba06
TheRealRobis <retasas.ruskys@gmail.com>
2019-11-07 14:30:45 +0200

    masyvas nr 1

--- index.php
+++ index.php
@@ -1,34 +1,7 @@
 <?php
-$speed_of_sound = 333;
-$db_decr = 6;
-$db = 120;
-$sec = 0;
-$p = '';
-$h3 = '';
-$my_position = rand(1, 100);
+$array = ['Kiausinis', 'Baklazanas', 'Obuolys'];
 
-for ($dist = 1; $db > 0; $dist *= 2) {
-    $sec = round($dist / $speed_of_sound);
-    $db -= $db_decr;
-    $p .= "Po $sec s ($dist m.): $db db<br>";
-
-
-//    if ($db > 90 && $my_position <= $dist) {
-//        $h3 .= "Stovedamas $my_position m. nuo griaustinio keisiu kelnes. ";
-//    } else {
-//        $h3 .= "Stovedamas $my_position m. nuo griaustinio nekeisiu kelnes. ";
-//    }
-    if($db > 90){
-        if($my_position <= $dist){
-            $h3 .= "Stovedamas $my_position m. nuo griaustinio keisiu kelnes. ";
-        } else {
-             $h3 .= "Stovedamas $my_position m. nuo griaustinio nekeisiu kelnes. ";
-    }
-}
-}
-echo $my_position;
-
-$h1 = 'Griaustinio zona';
+var_dump($array);
 ?>
 <html>
     <head>
@@ -36,9 +9,6 @@ $h1 = 'Griaustinio zona';
         <title>PHP</title>           
     </head>
     <body>
-        <h1><?php print $h1; ?></h1>
-        <p><?php print $p; ?></p>
-        <h3><?php print $h3; ?></h3>
     </body>
 </html>
 
