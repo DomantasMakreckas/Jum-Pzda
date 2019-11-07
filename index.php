10adf8c7005eb06f743e7437f3ad93c300e4212f
TheRealRobis <retasas.ruskys@gmail.com>
2019-11-07 16:34:19 +0200

    Boulingas

--- index.php
+++ index.php
@@ -1,42 +1,42 @@
 <?php
 
 $array = [
-    [
+// PIRMAS EILE
+    [  
         [
-            'pavadinimas' => 'Kiausiniai',
-            'kiekis' => 3,
+            'boulingo_takelio_busena' => 'Neprivemta',
         ],
+//  Kegliu busena
+// Kegliu eiles prasideda nuo pirmo matoto
+// Kegliai skaiciuojami nuo kaires i desine
         [
-            'pavadinimas' => 'Grietine',
-            'kiekis' => 1,
-        ],
-        [
-            'pavadinimas' => 'Jogurtas',
-            'kiekis' => 2,
-        ],
-        [
-            'pavadinimas' => 'Zuvis',
-            'kiekis' => 0,
-        ],
-    ],
-    [
-        [
-            'pavadinimas' => 'Pienas',
-            'kiekis' => 1,
-        ],
-        [
-            'pavadinimas' => 'Kefyras',
-            'kiekis' => 0,
-        ],
-        [
-            'pavadinimas' => 'Degtine',
-            'kiekis' => 2,
-        ],
-        [
-            'pavadinimas' => 'Kecupas',
-            'kiekis' => 2,
+           'pirma_eile' => [
+                'pirmas_keglis' => true,
+            ],
+            'antra_eile' => [
+                'antras_keglis' => false,
+                'trecias_keglis' => true,
+            ],
+            'trecia_eile' => [
+                'ketvirtas_keglis' => false,
+                'penktas_keglis' => false,
+                'sestas_keglis' => true,
+            ],
+            'ketvirta_eile' => [
+                'septintas_keglis' => true,
+                'astuntas_keglis' => false,
+                'devintas_keglis' => false,
+                'desimtas_keglis' => true,
+            ],
         ],
     ],
+//  ANTRA EILE
+    [
+        [
+            'boulingo_takelio_busena' => 'Privemta',
+        ], 
+        
+    ]
 ];
 var_dump($array);
 
