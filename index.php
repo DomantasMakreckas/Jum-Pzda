894a15303f056f22f59155c2da0b8da56dd6b56c
TheRealRobis <retasas.ruskys@gmail.com>
2019-11-07 17:30:41 +0200

    panda

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 $array = [
     'vieta'  => 'gatve bbz kur',
     'laikas' => 'vakare nes saules nera',
-    'padaras' => 'liudna panda',
+    'padaras' => 'liudna panda erotineje pozicijoje',
 ];
  
 
