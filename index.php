901afe1103f3b780559579ae39a4d56b1dc6a5d4
TheRealRobis <retasas.ruskys@gmail.com>
2019-11-08 10:36:42 +0200

    Index.php

--- index.php
+++ index.php
@@ -1,11 +1,1 @@
-<?php
-
-$array = [
-    'vieta'  => 'gatve bbz kur',
-    'laikas' => 'vakare nes saules nera',
-    'padaras' => 'liudna panda erotineje pozicijoje',
-];
- 
-
-var_dump($array);
-
+<?php ;?>
