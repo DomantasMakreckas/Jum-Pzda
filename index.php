2174354c138d1f9dbd5cf06c12758605aefc714a
Ugnius <ugniusnaujokas@gmail.com>
2019-11-08 10:33:21 +0200

    labas

--- index.php
+++ index.php
@@ -1,29 +1,341 @@
 <?php
 $trasa = [
-        [ // pirmas
-            'kryptis' => 'desine',
-            'laipsniai_kryptimi' => 180
-        ],
-        [ // antras
-            'kryptis' => 'kaire',
-            'laipsniai_kryptimi' => 20
-        ], // trecias
-                [
-            'kryptis' => 'desine',
-            'laipsniai_kryptimi' => 90
-        ],
-        [ // ketvirtas
-            'kryptis' => 'kaire',
-            'laipsniai_kryptimi' => 180
-        ],
-        [ // penktas
-            'kryptis' => 'desine',
-            'laipsniai_kryptimi' => 180
-        ], 
-                [// sestas
-            'kryptis' => 'desine',
-            'laipsniai_kryptimi' => 120
-        ]
+    // pirma eile
+    [
+        ['yellow' => false,
+         'green' => true,
+         'red' => false,
+         'off' => false],
+        
+        ['yellow' => false,
+         'green' => true,
+         'red' => false,
+         'off' => false],
+        
+        ['yellow' => false,
+         'green' => true,
+         'red' => false,
+         'off' => false],
+        
+        ['yellow' => false,
+         'green' => true,
+         'red' => false,
+         'off' => false],
+        
+        ['yellow' => false,
+         'green' => true,
+         'red' => false,
+         'off' => false],
+        
+        ['yellow' => false,
+         'green' => true,
+         'red' => false,
+         'off' => false],
+        
+        ['yellow' => false,
+         'green' => true,
+         'red' => false,
+         'off' => false],
+        
+        ['yellow' => false,
+         'green' => true,
+         'red' => false,
+         'off' => false]
+    ],
+    //antra eile
+    [
+         ['yellow' => false,
+         'green' => true,
+         'red' => false,
+         'off' => false],
+        
+        ['yellow' => true,
+         'green' => false,
+         'red' => false,
+         'off' => false],
+        
+        ['yellow' => true,
+         'green' => false,
+         'red' => false,
+         'off' => false],
+        
+        ['yellow' => true,
+         'green' => false,
+         'red' => false,
+         'off' => false],
+        
+        ['yellow' => true,
+         'green' => false,
+         'red' => false,
+         'off' => false],
+        
+        ['yellow' => true,
+         'green' => false,
+         'red' => false,
+         'off' => false],
+        
+        ['yellow' => true,
+         'green' => false,
+         'red' => false,
+         'off' => false],
+        
+        ['yellow' => false,
+         'green' => true,
+         'red' => false,
+         'off' => false]
+    ],
+    //trecia eile
+    [
+         ['yellow' => false,
+         'green' => true,
+         'red' => false,
+         'off' => false],
+        
+        ['yellow' => true,
+         'green' => false,
+         'red' => false,
+         'off' => false],
+        
+        ['yellow' => false,
+         'green' => false,
+         'red' => true,
+         'off' => false],
+        
+        ['yellow' => false,
+         'green' => false,
+         'red' => true,
+         'off' => false],
+        
+        ['yellow' => false,
+         'green' => false,
+         'red' => true,
+         'off' => false],
+        
+        ['yellow' => false,
+         'green' => false,
+         'red' => true,
+         'off' => false],
+        
+        ['yellow' => true,
+         'green' => false,
+         'red' => false,
+         'off' => false],
+        
+        ['yellow' => false,
+         'green' => true,
+         'red' => false,
+         'off' => false]
+    ],
+    //ketvirta
+    [
+         ['yellow' => false,
+         'green' => true,
+         'red' => false,
+         'off' => false],
+        
+        ['yellow' => true,
+         'green' => false,
+         'red' => false,
+         'off' => false],
+        
+        ['yellow' => false,
+         'green' => false,
+         'red' => true,
+         'off' => false],
+        
+        ['yellow' => true,
+         'green' => false,
+         'red' => false,
+         'off' => false],
+        
+        ['yellow' => true,
+         'green' => false,
+         'red' => false,
+         'off' => false],
+        
+        ['yellow' => false,
+         'green' => false,
+         'red' => true,
+         'off' => false],
+        
+        ['yellow' => true,
+         'green' => false,
+         'red' => false,
+         'off' => false],
+        
+        ['yellow' => false,
+         'green' => true,
+         'red' => false,
+         'off' => false]
+    ],
+    // penkta eile
+    [
+         ['yellow' => false,
+         'green' => true,
+         'red' => false,
+         'off' => false],
+        
+        ['yellow' => true,
+         'green' => false,
+         'red' => false,
+         'off' => false],
+        
+        ['yellow' => false,
+         'green' => false,
+         'red' => true,
+         'off' => false],
+        
+        ['yellow' => true,
+         'green' => false,
+         'red' => false,
+         'off' => false],
+        
+        ['yellow' => true,
+         'green' => false,
+         'red' => false,
+         'off' => false],
+        
+        ['yellow' => false,
+         'green' => false,
+         'red' => true,
+         'off' => false],
+        
+        ['yellow' => true,
+         'green' => false,
+         'red' => false,
+         'off' => false],
+        
+        ['yellow' => false,
+         'green' => true,
+         'red' => false,
+         'off' => false]
+    ],
+    // sesta eile
+    [
+         ['yellow' => false,
+         'green' => true,
+         'red' => false,
+         'off' => false],
+        
+        ['yellow' => true,
+         'green' => false,
+         'red' => false,
+         'off' => false],
+        
+        ['yellow' => false,
+         'green' => false,
+         'red' => true,
+         'off' => false],
+        
+        ['yellow' => false,
+         'green' => false,
+         'red' => true,
+         'off' => false],
+        
+        ['yellow' => false,
+         'green' => false,
+         'red' => true,
+         'off' => false],
+        
+        ['yellow' => false,
+         'green' => false,
+         'red' => true,
+         'off' => false],
+        
+        ['yellow' => true,
+         'green' => false,
+         'red' => false,
+         'off' => false],
+        
+        ['yellow' => false,
+         'green' => true,
+         'red' => false,
+         'off' => false]
+    ],
+    // septinta eile
+    [
+                ['yellow' => false,
+         'green' => true,
+         'red' => false,
+         'off' => false],
+        
+        ['yellow' => true,
+         'green' => false,
+         'red' => false,
+         'off' => false],
+        
+        ['yellow' => true,
+         'green' => false,
+         'red' => false,
+         'off' => false],
+        
+        ['yellow' => true,
+         'green' => false,
+         'red' => false,
+         'off' => false],
+        
+        ['yellow' => true,
+         'green' => false,
+         'red' => false,
+         'off' => false],
+        
+        ['yellow' => true,
+         'green' => false,
+         'red' => false,
+         'off' => false],
+        
+        ['yellow' => true,
+         'green' => false,
+         'red' => false,
+         'off' => false],
+        
+        ['yellow' => false,
+         'green' => true,
+         'red' => false,
+         'off' => false]
+    ],
+    // astunta eile
+    [
+         ['yellow' => false,
+         'green' => true,
+         'red' => false,
+         'off' => false],
+        
+        ['yellow' => false,
+         'green' => true,
+         'red' => false,
+         'off' => false],
+        
+        ['yellow' => false,
+         'green' => true,
+         'red' => false,
+         'off' => false],
+        
+        ['yellow' => false,
+         'green' => true,
+         'red' => false,
+         'off' => false],
+        
+        ['yellow' => false,
+         'green' => true,
+         'red' => false,
+         'off' => false],
+        
+        ['yellow' => false,
+         'green' => true,
+         'red' => false,
+         'off' => false],
+        
+        ['yellow' => false,
+         'green' => true,
+         'red' => false,
+         'off' => false],
+        
+        ['yellow' => false,
+         'green' => true,
+         'red' => false,
+         'off' => false]
+    ]
     ];
 var_dump($trasa);
 ?>
