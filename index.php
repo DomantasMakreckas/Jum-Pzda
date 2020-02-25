7259e63bcf649f3d0c8bdf5cba87572277c197a2
DomantasMakreckas <57089666+DomantasMakreckas@users.noreply.github.com>
2020-02-25 09:36:37 +0200

    coinas

--- index.php
+++ index.php
@@ -1,20 +1,46 @@
 <?php
-$data = date('Y m d h:i:s');
-$data_tekstas = "$data Domantas grįžo į PHPFIGHTCLUB'ą!";
-$atgal = date('F', strtotime('-1 month'));
-$atgal_tekstas = "$atgal prieme teisingą sprendimą";
-$kraujas = rand(20, 30) / 10;
-$kraujas_tekstas = "Per sia sekunde organizmas pagamino $kraujas mil. kraujo lasteliu";
+$foto = rand(0, 1);
+if ($foto) {
+    $bg = 'Priekis';
+} else {
+    $bg = 'Galas';
+}
 ?>
+
+<style>
+    .priekis {
+        height: 100vh;
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-image: url(https://www.just2euro.lt/wp-content/uploads/2019/09/2-euro-coin-2019-Lithuania-Zemaitija.jpg);
+    }
+
+    .galas {
+        background-image: url(https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT2NG_D1FxvmRbnJ5P5XacnxPI8D3GW922S7rEQlvKmkC8P20BV);
+        height: 100vh;
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+    }
+
+    div {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        color: red;
+        font-size: 80px;
+    }
+</style>
 
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Lenta | PHP Fight Club</title>
+        <title>Coin-Flip</title>
     </head>
     <body>
-        <p><?php print $data_tekstas ?></p>
-        <p><?php print $atgal_tekstas ?></p>
-        <p><?php print $kraujas_tekstas ?> </p>
+        <div class="<?php print $bg ?>">
+            <span><?php print $bg ?></span>
+        </div>
     </body>
 </html>
