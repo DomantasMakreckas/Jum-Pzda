cab6c5ce70898c58c894174b35830adc673db1ce
DomantasMakreckas <57089666+DomantasMakreckas@users.noreply.github.com>
2020-03-02 10:47:39 +0200

    mentai

--- index.php
+++ index.php
@@ -1,2 +1,27 @@
 <?php
-asdga
+$police_fuel = rand(50, 100);
+$police_cons = 7.5;
+$my_fuel = rand(40, 150);
+$my_cons = 11.5;
+
+$p_distance = round($police_fuel / $police_cons * 100, 2);
+$m_distance = round($my_fuel / $my_cons * 100, 2);
+
+$li1 = "Farai nuvažiuotų: $p_distance km";
+$li2 = "Aš nuvažiuočiau: $m_distance km";
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="utf-8">
+    <title></title>
+</head>
+<body>
+<h1>Pabėgimo skaičiuoklė</h1>
+<ul>
+    <li><?php print $li1 ?></li>
+    <li><?php print $li2 ?></li>
+</ul>
+</body>
+</html>
