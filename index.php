fb546fc13612576447a10e78a9c994f67c5078f8
DomantasMakreckas <57089666+DomantasMakreckas@users.noreply.github.com>
2020-03-10 10:37:09 +0200

    nav

--- index.php
+++ index.php
@@ -1,33 +1,7 @@
 <?php
 $title = 'Banko israsas';
 
-$bank_reports = [
-    [
-        'name' => 'Alga',
-        'amount' => 600
-    ],
-    [
-        'name' => 'Nacnykas',
-        'amount' => -30
-    ],
-    [
-        'name' => 'Maxima',
-        'amount' => -20
-    ],
-    [
-        'name' => 'Nacnykas',
-        'amount' => -15
-    ]
-];
 
-foreach ($bank_reports as $key => $report) {
-
-    if ($report['amount'] > 0) {
-        $bank_reports[$key]['budget'] = 'income';
-    } else {
-        $bank_reports[$key]['budget'] = 'expense';
-    }
-}
 
 ?>
 <!DOCTYPE html>
@@ -37,22 +11,9 @@ foreach ($bank_reports as $key => $report) {
     <title><?php print $title ?></title>
 </head>
 <style>
-    .income {
-        color: green;
-    }
 
-    .expense {
-        color: red;
-    }
 </style>
 <body>
-<h1>Banko israsas</h1>
-<ul>
-    <?php foreach ($bank_reports as $report): ?>
-        <li class="<?php print $report['budget'] ?>"><?php print $report['name'] ?> :
-            <?php print $report['amount'] ?>
-        </li>
-    <?php endforeach; ?>
-</ul>
+
 </body>
 </html>
