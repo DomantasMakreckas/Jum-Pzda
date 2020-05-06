32f52d037bffc7aa2a7b0fd86b31ecbffee9cc7b
DomantasMakreckas <57089666+DomantasMakreckas@users.noreply.github.com>
2020-05-06 09:52:51 +0300

    js lyrics

--- app.js
+++ app.js
@@ -1,23 +1,88 @@
-const h1 = document.getElementById('title');
-const h2 = document.querySelector('.lentele');
+'use strict';
 
-const input =  prompt('kas bus h2?');
+// const shoppingCart = {
+//     obuolys: 2,
+//     bananas: 3
+// };
+//
+// let answer;
+//
+// do {
+//     const productName = prompt('Iveskite norima produkta');
+//     const productQuantity = prompt('Iveskite skaiciu kiek noresite');
+//
+//     shoppingCart[productName] = productQuantity;
+//
+//     answer = prompt('Dar kazko?');
+// }
+// while (answer === 'noriu');
+//
+// for (let key in shoppingCart) {
+//     console.log(key + '-kiekis = ' + shoppingCart[key]);
+//     // console.log(`${key} - kiekis = ${shoppingCart[key]}`);  veikia ir taip
+// }
+//
+// console.log(shoppingCart);
 
-h2.innerHTML = input;
 
+const rapLyrics = [
+    {
+        word: 'mergos',
+        naughty: false,
+    },
+    {
+        word: 'narkotikai',
+        naughty: true,
+    },
+    {
+        word: 'bapkes',
+        naughty: false,
+    },
+    {
+        word: 'bmw',
+        naughty: false,
+    },
+    {
+        word: 'mergos',
+        naughty: false,
+    },
+    {
+        word: 'narkotikai',
+        naughty: true,
+    },
+    {
+        word: 'bapkes',
+        naughty: false,
+    },
+    {
+        word: 'bmw',
+        naughty: false,
+    },
+];
 
-const div = document.createElement('div');
-const span = document.createElement('span');
-const text = document.createTextNode('kazkoks tekstas ');
-const text2 = document.createTextNode('kazkoks tekstas13 ');
+let age = prompt('Iveskite savo amziu');
+// let search = prompt('Kokio zodzio ieskai?');
+// let find = rapLyrics.find(getLyrics);
+// let filter = rapLyrics.filter(getLyrics);
 
-span.appendChild(text2);
-div.appendChild(text);
+const result = age < 17 ?
+    rapLyrics.filter(censor) :
+    rapLyrics;
 
-document.body.appendChild(span);
-document.body.appendChild(div);
+function censor(value) {
+    return !value.naughty;
+}
 
-console.log(div);
+function getWords(value) {
+    return value.word;
+}
 
+const lyrics = result.map(getWords);
 
+console.log(result);
+console.log(result.map(getWords));
 
+const p = document.createElement('p');
+const text = document.createTextNode(lyrics.join(' '));
+p.appendChild(text);
+document.body.appendChild(p);
