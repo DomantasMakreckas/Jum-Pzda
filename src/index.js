2d7298e0948bf8ae26050f3016cf25a90b5db6d6
DomantasMakreckas <57089666+DomantasMakreckas@users.noreply.github.com>
2020-05-21 11:22:05 +0300

    cigaru pakelis

--- src/index.js
+++ src/index.js
@@ -1,17 +1,42 @@
 'use script';
+//
+// import {hyperscript} from "./hyperscript";
+// import {mount} from "./mount";
+// import {App} from "./App";
+//
+// const button = hyperscript('button', {class: 'btn'}, 'ciulpiu');
+// const h1 = hyperscript('h1', {class: 'title'}, 'Bybys');
+// const div = hyperscript('div', {class: 'container', style: 'height: 200px'}, h1, button);
+//
+// mount(hyperscript(App),
+//     document.getElementById('app')
+// );
 
-import {hyperscript} from "./hyperscript";
-import {mount} from "./mount";
-import {App} from "./App";
+const pakelis = {
+    vnt: 20,
+    parukyti() {
+        this.vnt--;
+        console.log('likutis ' + this.vnt + ' vnt');
+        return this.vnt;
+    }
+};
 
-const button = hyperscript('button', {class: 'btn'}, 'ciulpiu');
-const h1 = hyperscript('h1', {class: 'title'}, 'Bybys');
-const div = hyperscript('div', {class: 'container', style: 'height: 200px'}, h1, button);
+// function surukyti() {
+//     while (pakelis.vnt > 0) {
+//         pakelis.parukyti();
+//     }
+// }
 
-mount(hyperscript(App),
-    document.getElementById('app')
-);
+// surukyti();
 
+const mygtukas = document.createElement('button');
+const tekstas = document.createTextNode('rukyti');
+mygtukas.appendChild(tekstas);
+document.body.appendChild(mygtukas);
 
+pakelis.parukyti = pakelis.parukyti.bind(pakelis);
+mygtukas.addEventListener('click', pakelis.parukyti);
 
+const fn = pakelis.parukyti.bind(pakelis);
 
+console.log(typeof pakelis.parukyti)
